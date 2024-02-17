ad3fa5deeb5738a8338cca27f439b60ccec71f33
Rohan-J123 <rohanjha@aol.in>
2024-02-18 01:53:29 +0530

    Minor Bug Fixes

--- scripts/indexJS/toggle.js
+++ scripts/indexJS/toggle.js
@@ -1,4 +1,4 @@
-function toggleSidebar() {
+function toggleSidebarOn() {
     var mainbar = document.getElementById('mainbar');
     var sidebar = document.getElementById('sidebar');
 
@@ -14,9 +14,32 @@ function toggleSidebar() {
 
     var toggleButtonShowInfo = document.getElementById('toggle-button-to-show-info');
     toggleButtonShowInfo.style.pointerEvents = toggleButtonShowInfo.style.pointerEvents === 'none' ? 'auto' : 'none';
-    toggleButtonShowInfo.innerText = toggleButtonShowInfo.innerText === 'info' ? '' : 'info';
+    toggleButtonShowInfo.innerText ='';
 
     var toggleButtonHideInfo = document.getElementById('toggle-button-to-hide-info');
     toggleButtonHideInfo.style.pointerEvents = toggleButtonHideInfo.style.pointerEvents === 'none' ? 'auto' : 'none';
-    toggleButtonHideInfo.innerText = toggleButtonHideInfo.innerText === 'cancel' ? '' : 'cancel';
+    toggleButtonHideInfo.innerText = 'cancel';
+}
+
+function toggleSidebarOff() {
+    var mainbar = document.getElementById('mainbar');
+    var sidebar = document.getElementById('sidebar');
+
+    if(window.innerWidth < 600){
+        sidebar.style.transition = '0s';
+        mainbar.style.transition = '0s';
+    }else{
+        sidebar.style.transition = '1s';
+        mainbar.style.transition = 'margin-right 1s';
+    }
+
+    sidebar.style.width = sidebar.style.width === '600px' ? '0px' : '600px';
+
+    var toggleButtonShowInfo = document.getElementById('toggle-button-to-show-info');
+    toggleButtonShowInfo.style.pointerEvents = toggleButtonShowInfo.style.pointerEvents === 'none' ? 'auto' : 'none';
+    toggleButtonShowInfo.innerText = 'info';
+
+    var toggleButtonHideInfo = document.getElementById('toggle-button-to-hide-info');
+    toggleButtonHideInfo.style.pointerEvents = toggleButtonHideInfo.style.pointerEvents === 'none' ? 'auto' : 'none';
+    toggleButtonHideInfo.innerText = '';
 }
