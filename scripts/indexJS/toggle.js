affacb0f55e9e944b3c691269f2ab9d29e594be2
Rohan-J123 <rohanjha@aol.in>
2024-02-18 01:10:10 +0530

    SideBar closing and opening operations added

--- scripts/indexJS/toggle.js
+++ scripts/indexJS/toggle.js
@@ -2,9 +2,11 @@ function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     sidebar.style.width = sidebar.style.width === '600px' ? '0px' : '600px';
 
-    var toggleButton = document.getElementById('toggle-button');
-    toggleButton.innerText = toggleButton.innerText === 'cancel' ? 'info' : 'cancel';
+    var toggleButtonShowInfo = document.getElementById('toggle-button-to-show-info');
+    toggleButtonShowInfo.innerText = toggleButtonShowInfo.innerText === 'info' ? '' : 'info';
+    toggleButtonShowInfo.style.pointerEvents = toggleButtonShowInfo.style.pointerEvents === 'none' ? 'auto' : 'none';
 
-    // var toggleImage = document.getElementById('toggle-image');
-    // toggleImage.style.width = toggleImage.style.width === '45%' ? '0%' : '45%';
+    var toggleButtonHideInfo = document.getElementById('toggle-button-to-hide-info');
+    toggleButtonHideInfo.innerText = toggleButtonHideInfo.innerText === 'cancel' ? '' : 'cancel';
+    toggleButtonHideInfo.style.pointerEvents = toggleButtonHideInfo.style.pointerEvents === 'none' ? 'auto' : 'none';
 }
