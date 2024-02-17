ebfc4b8f570b988e8a7fc64bc09f7a8b65fc7e63
Rohan-J123 <rohanjha@aol.in>
2024-02-17 23:26:44 +0530

    Minor Changes

--- scripts/indexJS/toggle.js
+++ scripts/indexJS/toggle.js
@@ -3,7 +3,7 @@ function toggleSidebar() {
     sidebar.style.width = sidebar.style.width === '600px' ? '0px' : '600px';
 
     var toggleButton = document.getElementById('toggle-button');
-    toggleButton.innerText = toggleButton.innerText === '>' ? '<' : '>';
+    toggleButton.innerText = toggleButton.innerText === 'cancel' ? 'info' : 'cancel';
 
     // var toggleImage = document.getElementById('toggle-image');
     // toggleImage.style.width = toggleImage.style.width === '45%' ? '0%' : '45%';
