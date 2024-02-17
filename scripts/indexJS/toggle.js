9e3830602585784424e88c30b3883a12734befa7
Rohan-J123 <rohanjha@aol.in>
2024-02-17 19:55:29 +0530

    Added Instructions

--- scripts/indexJS/toggle.js
+++ scripts/indexJS/toggle.js
@@ -1,6 +1,6 @@
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
-    sidebar.style.width = sidebar.style.width === '35vw' ? '0vw' : '35vw';
+    sidebar.style.width = sidebar.style.width === '600px' ? '0px' : '600px';
 
     var toggleButton = document.getElementById('toggle-button');
     toggleButton.innerText = toggleButton.innerText === '>' ? '<' : '>';
