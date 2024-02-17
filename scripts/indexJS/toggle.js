9102e74112869855d768b22a90a9c43869e9f455
Rohan-J123 <rohanjha@aol.in>
2024-02-18 01:46:55 +0530

    Minor Changes

--- scripts/indexJS/toggle.js
+++ scripts/indexJS/toggle.js
@@ -13,10 +13,10 @@ function toggleSidebar() {
     sidebar.style.width = sidebar.style.width === '600px' ? '0px' : '600px';
 
     var toggleButtonShowInfo = document.getElementById('toggle-button-to-show-info');
-    toggleButtonShowInfo.innerText = toggleButtonShowInfo.innerText === 'info' ? '' : 'info';
     toggleButtonShowInfo.style.pointerEvents = toggleButtonShowInfo.style.pointerEvents === 'none' ? 'auto' : 'none';
+    toggleButtonShowInfo.innerText = toggleButtonShowInfo.innerText === 'info' ? '' : 'info';
 
     var toggleButtonHideInfo = document.getElementById('toggle-button-to-hide-info');
-    toggleButtonHideInfo.innerText = toggleButtonHideInfo.innerText === 'cancel' ? '' : 'cancel';
     toggleButtonHideInfo.style.pointerEvents = toggleButtonHideInfo.style.pointerEvents === 'none' ? 'auto' : 'none';
+    toggleButtonHideInfo.innerText = toggleButtonHideInfo.innerText === 'cancel' ? '' : 'cancel';
 }
