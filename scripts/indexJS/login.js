608fdc74af73f51087fe5b30584e40ecf59729cc
Rohan-J123 <rohanjha@aol.in>
2024-05-15 02:50:45 +0530

    Different branch

--- scripts/indexJS/login.js
+++ scripts/indexJS/login.js
@@ -7,18 +7,8 @@ document.getElementById("login-info").addEventListener("submit", function(event)
     document.getElementById('spinner-circle').style.display = 'block';
     const name = document.getElementById("login-name").value;
     const email = document.getElementById("login-email").value;
-    const field = document.getElementById("login-field").value;
     const accessibilityKnowledge = document.getElementById("login-accessibilty-knowledge").value;
     const area = document.getElementById("login-area").value;
-
-    var subfield='';
-    switch(document.getElementById("login-field").value){
-        case 'High School Student': subfield = document.getElementById('subfield-highschool').value; break;
-        case 'Undergraduate Student': subfield = document.getElementById('subfield-undergrad').value; break;
-        case 'Graduate Student': subfield = document.getElementById('subfield-grad').value; break;
-        case 'Working Professional': subfield = document.getElementById('subfield-worker').value; break;
-        case 'Other': subfield = document.getElementById('subfield-other').value; break;
-    }
 
     var userId = db.collection("users").doc().id;
     console.log(typeof userId);
@@ -28,10 +18,8 @@ document.getElementById("login-info").addEventListener("submit", function(event)
         db.collection("users").doc(userId).set({
             name: name,
             email: email,
-            field: field,
             accessibilityKnowledge: accessibilityKnowledge,
-            area: area,
-            subfield: subfield
+            area: area
         })
         .then(function() {
             console.log("Document successfully written with ID: ", userId);
@@ -56,25 +44,6 @@ function accountOpen(){
     document.getElementById('account-email').innerText = "Email: " + sessionStorage.getItem('user-email');
     document.getElementById('account-score').innerText = document.getElementById('score-text').innerText;
 }
-
-document.getElementById('login-field').addEventListener('change', function() {
-    var selectedValue = '';
-    switch (this.value) {
-        case 'High School Student': selectedValue = 'subfield-highschool'; break;
-        case 'Undergraduate Student': selectedValue = 'subfield-undergrad'; break;
-        case 'Graduate Student': selectedValue = 'subfield-grad'; break;
-        case 'Working Professional': selectedValue = 'subfield-worker'; break;
-        case 'Other': selectedValue = 'subfield-other'; break;
-    };
-    var dropdowns = document.getElementById('sub-field-dropdowns').children;
-    
-    for (var i = 0; i < dropdowns.length; i++) {
-        dropdowns[i].style.display = 'none';    
-    }
-
-    document.getElementById(selectedValue).style.display = 'block';
-    document.getElementById(selectedValue + '-label').style.display = 'block';
-});
 
 document.addEventListener('DOMContentLoaded', (event) => {
     const checkbox = document.getElementById('login-video-understand-button');
