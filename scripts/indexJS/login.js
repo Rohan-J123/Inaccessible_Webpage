1776fe204665ede72348c4d70b094c074f965544
Rohan-J123 <rohanjha@aol.in>
2024-05-16 17:44:36 +0530

    Added Timestamp to db

--- scripts/indexJS/login.js
+++ scripts/indexJS/login.js
@@ -11,6 +11,12 @@ document.getElementById("login-info").addEventListener("submit", function(event)
     const accessibilityKnowledge = document.getElementById("login-accessibilty-knowledge").value;
     const area = document.getElementById("login-area").value;
 
+    // Calculate current IST time
+    const now = new Date();
+    const utcOffset = now.getTimezoneOffset() * 60000; // Get the current UTC offset in milliseconds
+    const istOffset = 5.5 * 60 * 60 * 1000; // IST offset in milliseconds
+    const istTime = new Date(now.getTime() + utcOffset + istOffset); // Adjust current time to IST    
+
     var subfield='';
     switch(document.getElementById("login-field").value){
         case 'High School Student': subfield = document.getElementById('subfield-highschool').value; break;
@@ -31,7 +37,8 @@ document.getElementById("login-info").addEventListener("submit", function(event)
             field: field,
             accessibilityKnowledge: accessibilityKnowledge,
             area: area,
-            subfield: subfield
+            subfield: subfield,
+            timestamp: String(istTime)
         })
         .then(function() {
             console.log("Document successfully written with ID: ", userId);
